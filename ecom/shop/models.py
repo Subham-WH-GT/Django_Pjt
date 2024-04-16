83296cd8538d6f431f6dd2ccd232c2e88d08632b
Subham-WH-GT <awandboy685@gmail.com>
2024-04-16 22:44:08 +0530

    updated

--- ecom/shop/models.py
+++ ecom/shop/models.py
@@ -5,5 +5,14 @@ class Product(models.Model):
     product_id=models.AutoField
     product_name=models.CharField(max_length=50)
     desc=models.CharField(max_length=300)
+    category=models.CharField(max_length=50,default="")
+    subcategory=models.CharField(max_length=50,default="")
+    price=models.IntegerField(default=0)
+    image=models.ImageField(upload_to="shop/images",default="")
     pub_date=models.DateField()
+
+    def __str__(self):
+        return self.product_name
+    
+
 
